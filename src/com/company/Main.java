b78ad63f96ab3a301ccf7e38589e8872465dd6c2
Diansuci6706164048 <37732513+Diansuci6706164048@users.noreply.github.com>
2018-03-29 09:24:15 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         for(int x=n; x>=1; x--)
         {
             for(int y=x-1; y>=1; y--){
-                System.out.print(" ");
+                System.out.print("o");
             }
             for(int z=1; z<=((n+1)-x)*2; z++){
                 System.out.print("*");
@@ -25,7 +25,7 @@ public class Main {
         for(int i=n; i>=1; i--)
         {
             for(int k=1; k<=n-i; k++){
-                System.out.print(" ");
+                System.out.print("o");
             }
             for(int j=i*2; j>=1; j--){
                 System.out.print("*");
